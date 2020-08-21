32f15dd6d6de406a940b614b51db2768c1828c87
Hugo Santiago Hernandez Limas <hugohl98.hg.gl@gmail.com>
2020-08-21 14:25:22 -0500

    Animations added Beta1.1.0

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -79,9 +79,46 @@ struct MemoryGame <CardContent> where CardContent: Equatable {
     struct Card: Identifiable {
         var id: Int
         
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
+        var isFaceUp: Bool = false { didSet { isFaceUp ? startUsinBonusTime() : stopUsingBonusTIme() } }
+        var isMatched: Bool = false { didSet { stopUsingBonusTIme() } }
         var content: CardContent
         
+        var bonusTimeLimit : TimeInterval = 6
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        var lastFaceUpDate : Date?
+        var pastFaceUpTime : TimeInterval = 0
+        
+        var bonusTimeRemaining : TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        
+        var bonusRemaining : Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining : 0
+        }
+        
+        var hasEarnedBonus : Bool {
+            isMatched && bonusTimeRemaining > 0
+        }
+        
+        var isConsumingBonusTime : Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        private mutating func startUsinBonusTime(){
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        private mutating func stopUsingBonusTIme(){
+            pastFaceUpTime = faceUpTime
+            self.lastFaceUpDate = nil
+        }
     }
 }
--- Memorize/View/GameView.swift
+++ Memorize/View/GameView.swift
@@ -17,12 +17,14 @@ struct GameView: View {
         Group {
             
             Button(action: {
-                self.viewModel.resetGame()
+                withAnimation(.easeInOut){
+                    self.viewModel.resetGame()
+                }
             }) {
                 HStack {
                     Image(systemName: "repeat")
                         .font(.title)
-                    Text("Repeat")
+                    Text("Reset")
                         .fontWeight(.semibold)
                         .font(.title)
                 }.foregroundColor(color)
@@ -30,8 +32,14 @@ struct GameView: View {
             
             Grid(viewModel.cards) {card in
                 CardView(card : card)
-                    .onTapGesture{ self.viewModel.choose(card:card) }
-                    .padding(5)
+                    .onTapGesture{
+                        withAnimation(.linear(duration: 0.6)){
+                            self.viewModel.choose(card:card)
+                            
+                        }
+                }
+                    
+                .padding(5)
             }
             .padding()
             .foregroundColor(color)
@@ -50,9 +58,17 @@ struct GameView: View {
 
 struct CardView: View {
     var card: MemoryGame<String>.Card
+    @State private var animatedBonusRemainig:Double = 0
+    
     var body : some View {
         GeometryReader{ geometry in
             self.body(for: geometry.size)
+        }
+    }
+    private func startAnimationTime(){
+        animatedBonusRemainig = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)){
+            animatedBonusRemainig = 0
         }
     }
     
@@ -60,15 +76,27 @@ struct CardView: View {
     func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockWise: true)
-                    .padding(5)
-                    .opacity(0.35)
+                if card.isConsumingBonusTime {
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemainig*360-90), clockWise: true)
+                        .padding(5)
+                        .opacity(0.35)
+                        .onAppear{
+                            self.startAnimationTime()
+                        }
+                } else {
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockWise: true)
+                        .padding(5)
+                        .opacity(0.35)
+                }
+                
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size) ))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false): .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.slide)
         }
-        
     }
     
     // MARK: - Drawing Constant
