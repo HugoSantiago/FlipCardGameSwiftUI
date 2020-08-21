94ed1b8e89bc27d62b256bf1f080fb196888b1b7
Hugo Santiago Hernandez Limas <hugohl98.hg.gl@gmail.com>
2020-08-21 09:56:20 -0500

    Access control added+viewModifiers

--- Memorize/View/GameView.swift
+++ Memorize/View/GameView.swift
@@ -56,28 +56,22 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     func body(for size: CGSize) -> some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke( lineWidth: edgeLineWidth)
+        if card.isFaceUp || !card.isMatched {
+            ZStack {
                 Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockWise: true)
                     .padding(5)
                     .opacity(0.35)
                 Text(card.content)
-            } else {
-                if !card.isMatched {
-                    RoundedRectangle(cornerRadius: cornerRadius).fill()
-                }
+                    .font(Font.system(size: fontSize(for: size) ))
             }
+            .cardify(isFaceUp: card.isFaceUp)
         }
-        .font(Font.system(size: fontSize(for: size) ))
+        
     }
     
     // MARK: - Drawing Constant
-    
-    private  let cornerRadius: CGFloat = 10
-    private  let edgeLineWidth: CGFloat = 3
     private  let fontScale: CGFloat = 0.7
     private  func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * fontScale
